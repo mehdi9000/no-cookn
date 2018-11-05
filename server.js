89e3aecc3b92ce0331d44e6e66badfc6f707a6ee
Folarin Farinto <folysmith@gmail.com>
2018-11-05 12:33:00 +0100

    changed folder structure

--- server.js
+++ server.js
@@ -53,10 +53,9 @@ app.use('/api/restaurants', restaurants);
 app.use('/api/restaurants-profile', restaurantsProfile);
 app.use('/api/category', category);
 
-// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
