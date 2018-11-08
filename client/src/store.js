d68d86ddca16d9c382e4627a0dcb570169cbac9f
Folarin Farinto <folysmith@gmail.com>
2018-11-08 09:45:04 +0100

    new store file

--- client/src/store.js
+++ client/src/store.js
@@ -1,35 +1,54 @@
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from './reducers';
+
+// const initialState = {};
+
+// const middleWare = [thunk];
+
+// var store;
+
+// if (process.env.NODE_ENV === 'production') {
+//   store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(applyMiddleware(...middleWare))
+//   );
+// } else {
+//   store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(
+//       applyMiddleware(...middleWare),
+//       window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//         window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+//   );
+// }
+
+// // const store = createStore(
+// //   rootReducer,
+// //   initialState,
+// //   compose(applyMiddleware(...middleWare))
+// // );
+
+// export default store;
+
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleWare = [thunk];
+const middleware = [thunk];
 
-var store;
-
-if (process.env.NODE_ENV === 'production') {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleWare))
-  );
-} else {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middleWare),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-}
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(applyMiddleware(...middleWare))
-// );
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
