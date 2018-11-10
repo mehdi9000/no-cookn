54516d3c7e3a7d8b85875f14f73cb892d8434838
Folarin Farinto <folysmith@gmail.com>
2018-11-10 07:23:22 +0100

    404 page remix

--- client/src/components/shared/404.js
+++ client/src/components/shared/404.js
@@ -9,16 +9,19 @@ const NotFound = () => {
         <div className="container">
           <div className="not-found">
             <div className="img-box">
-              <img src={BrokeBot} className="img-fluid" width="500px" />
+              <img src={BrokeBot} className="img-fluid" width="600px" />
             </div>
             <div className="text-box">
-              <h4 className="mt-5">Ooops! Something's Broken.</h4>
+              <h4 className="mt-5">Ooops! You broke a link.</h4>
               <h5 className="nt-3 mb-3">
                 The page you tried to visit does not exist on our server.
               </h5>
+              <h5 className="nt-3 mb-3">
+                Why not check out restaurants and discover amazing food.
+              </h5>
               <Link to="/">
                 <button className="btn btn-lg btn-warning">
-                  Back to Homepage
+                  Top Restaurants
                 </button>
               </Link>
             </div>
