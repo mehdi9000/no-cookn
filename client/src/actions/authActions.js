e05a2a133e8bbf48b8243be19270866ec69f65ae
Folarin Farinto <folysmith@gmail.com>
2019-06-23 21:23:41 +0100

    mods

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import setAuthToken from '../utils/setAuthToken';
-import jwt_decode from 'jwt-decode';
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post('api/users/register', userData)
-    .then(res => history.push('/welcome'))
+    .post("auth/register", userData)
+    .then(res => history.push("/welcome"))
     .catch(error => {
       dispatch({
         type: GET_ERRORS,
@@ -20,12 +20,12 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - Get User Token
 export const loginUser = userData => dispatch => {
   axios
-    .post('api/users/login', userData)
+    .post("auth/login", userData)
     .then(res => {
       // Save to localStorage
       const { token } = res.data;
       // Set token to ls
-      localStorage.setItem('jwtToken', token);
+      localStorage.setItem("jwtToken", token);
       // Set token to Auth header
       setAuthToken(token);
       // Decode token to get user data
@@ -46,8 +46,8 @@ export const VerifyUser = (activationcode, history) => dispatch => {
   axios
     .get(`http://localhost:4001/api/users/account/activate/${activationcode}`)
     .then(res => {
-      console.log('res', res.data);
-      setTimeout(history.push('/login'), 5000);
+      console.log("res", res.data);
+      setTimeout(history.push("/login"), 5000);
     })
     .catch(error => {
       dispatch({
@@ -68,10 +68,10 @@ export const setCurrentUser = decoded => {
 // Log user out
 export const logOutUser = () => dispatch => {
   // Remove token from localStorage
-  localStorage.removeItem('jwtToken');
+  localStorage.removeItem("jwtToken");
   // Remove auth header for future requests
   setAuthToken(false);
   // Set current user to {} which will set isAuthenticated to false
   dispatch(setCurrentUser({}));
-  window.location.href = '/login';
+  window.location.href = "/login";
 };
