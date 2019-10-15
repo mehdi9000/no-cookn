c18a5708f2a955a7bb7fc4a6bfcd1398b3c78cef
Folarin Farinto <Folysmith@gmail.com>
2019-10-15 14:41:29 +0100

    added restaurant profile routes and restaurant address routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,13 +7,17 @@ const userRoute = require('./user.routes');
 const profileRoute = require('./profile.routes');
 const addressRoute = require('./address.routes');
 const restaurantRoute = require('./restaurants.routes');
+const restaurantProfileRoute = require('./restautantProfile.routes');
+const restaurantAddress = require('./restaurantAddress.routes');
 
 router.use(userRoute);
 router.use(profileRoute);
 router.use(addressRoute);
 router.use(restaurantRoute);
+router.use(restaurantProfileRoute);
+router.use(restaurantAddress);
 
-router.get('/api', function(req, res, next) {
+router.get('/api', (req, res, next) => {
   res.status(200).json({ message: 'Shit Happens' });
 });
 
