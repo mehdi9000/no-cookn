096481b3dedf65d573ae49f8048fd8298ffa7479
Gboye <mujsann@gmail.com>
2020-02-18 06:39:19 -0500

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,7 @@
 const bcrypt = require("bcryptjs");
 const sendgrid = require("@sendgrid/mail");
-// const keys = require('../config/keys');
 sendgrid.setApiKey(process.env.sendGridApi);
 
-// const axios = require('axios');
 const Helpers = {};
 
 Helpers.HashValue = function HashValue(value) {
